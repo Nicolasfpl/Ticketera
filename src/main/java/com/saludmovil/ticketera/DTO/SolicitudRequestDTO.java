d60b5dda10bb4f2afa87cab39a782ea24f49cc01
Nicolas Palma <TU_CORREO_DE_GITHUB>
2026-09-08 19:31:33 -0300

    correccion DTO

--- src/main/java/com/saludmovil/ticketera/DTO/SolicitudRequestDTO.java
+++ src/main/java/com/saludmovil/ticketera/DTO/SolicitudRequestDTO.java
@@ -1,5 +1,24 @@
 package com.saludmovil.ticketera.DTO;
 
-public class SolicitudResquestDTO {
+import java.time.LocalDate;
+import java.util.List;
+
+import com.saludmovil.ticketera.model.HorarioVisita;
+import com.saludmovil.ticketera.model.Paciente;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data 
+@Builder 
+@AllArgsConstructor 
+@NoArgsConstructor 
+public class SolicitudRequestDTO {
+    private Paciente paciente;
+    private LocalDate fechaVisita;
+    private HorarioVisita horario;
+    private List<String> examenes;
 
 }
