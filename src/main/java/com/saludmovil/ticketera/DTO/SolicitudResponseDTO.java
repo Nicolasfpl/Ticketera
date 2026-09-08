b2711cd8847df67243e7f53f8b1b4d561b3a734a
Nicolas Palma <TU_CORREO_DE_GITHUB>
2026-09-08 19:41:02 -0300

    actualizar Response

--- src/main/java/com/saludmovil/ticketera/DTO/SolicitudResponseDTO.java
+++ src/main/java/com/saludmovil/ticketera/DTO/SolicitudResponseDTO.java
@@ -1,5 +1,29 @@
 package com.saludmovil.ticketera.DTO;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.saludmovil.ticketera.model.EstadoSolicitud;
+import com.saludmovil.ticketera.model.HorarioVisita;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data 
+@Builder 
+@AllArgsConstructor 
+@NoArgsConstructor 
 public class SolicitudResponseDTO {
+
+    private String numeroTicket;
+    private String nombrePaciente;
+    private LocalDate fechaVisita;
+    private HorarioVisita horario;
+    private List<String> examenes;
+    private EstadoSolicitud estado;
+    private String mensaje;
+    
 
 }
